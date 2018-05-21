c8c81b89b684d77511b5dc9b6c147b421bebc5db
zhangyaowei <zhangyaowei@dataocean.com.cn>
2018-05-22 01:02:05 +0800

    提交meeting页面

--- src/pages/meeting/meeting.ts
+++ src/pages/meeting/meeting.ts
@@ -20,12 +20,31 @@ export class MeetingPage {
         
   }
 
+  updataInfo (data) {
+    this.http.loadData('updateInfo', {publish_id: data, publish_get_username: this.userName}).subscribe(
+      res => {
+        let map = JSON.parse(res);
+        if (map.code === '0') {
+          console.log('暂无发布');
+        } else if (map.code === '1') { 
+          this.getData();
+        } else if (map.code === '2') {
+          console.log('服务器内部错误')
+        }
+      },
+      err => {
+
+      }
+    )
+  }
+
   getData() {
     this.http.loadData('publishNoAccept',{publish_code: '否'}).subscribe(
       res => {
         let map = JSON.parse(res);
         if (map.code === '0') {
           console.log('暂无发布');
+          this.publishInfo = [];
         } else if (map.code === '1') { 
           this.publishInfo = map.results;
           console.log(this.publishInfo);
